ea6caf1b6450120bb2903f8f0c89babd3a5318e5
mymusise <mymusise1@gmail.com>
2016-12-21 22:42:16 +0800

    add decorators
    Signed-off-by: mymusise <mymusise1@gmail.com>

--- ants/tests.py
+++ ants/tests.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
 from ants.utils import *
+from ants.decorators import *
 
 class ScriptTest(TestCase):
 
     def setUp(self):
         pass
+
+
+class DecoratorTest(TestCase):
+
+    def setUp(self):
+        def 
